b957f4e4ed8b2a463d16fd64ac4dd0e1baf429d8
EyupOzmen <ozmeneyup60@gmail.com>
2020-10-09 14:43:17 +0300

    "HTML5 Validation"

--- src/SurveyForm.js
+++ src/SurveyForm.js
@@ -3,8 +3,8 @@ import React, { useState,useEffect} from "react";
 const SurveyForm = () => {
 
   const [data,setData] = useState("")
-  const [min,setMin] = useState("")
-  const [max,setMax] = useState("")
+  let [min,setMin] = useState(1)
+  let [max,setMax] = useState(32)
 
   const [validation, setValidation] = useState("integer");
   const [shape, setShape] = useState({ step: 1, pattern: "[0-9]*" });
@@ -19,6 +19,9 @@ const SurveyForm = () => {
   const handleValidationChange = (event) => {
     const { value } = event.target;
 
+    //pattern olmayacak..
+    //step -- any
+    //aralık ilk değerler ..
     if (value === "integer" || value === "integer-range") {
       setShape({
         step: 1,
@@ -31,48 +34,32 @@ const SurveyForm = () => {
       });
     }
     setData("");
-    setMin("");
-    setMax("");
+    setMin(1);
+    setMax(32);
     setValidation(value);
     
   };
-
+  
   const handleDataChange = (event) => {
     const { value } = event.target;
-
-    setData(value);
+   
+       setData(value);
   };
 
   const handleMinChange = (event) => {
     const { value} = event.target;
-
+    
+    
     setMin(value)
   };
 
   const handleMaxChange = (event) => {
     const { value} = event.target;
-
-   setMax(value)
+   
+    setMax(value)
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setIsSubmitting(true);
-
-    const isValid = formValidation();
-    console.log(isValid)
-    if(isValid){
-      setIsSubmitting(false);
-      console.log(data)
-      
-      setData("");
-     setMin("");
-      setMax("");
-     
-    }
-    
-    setIsSubmitting(true)
-  };
+ 
 
   // useEffect(() => {
   //   formValidation()
@@ -80,96 +67,116 @@ const SurveyForm = () => {
   // },[data])
   
   const formValidation = () => {
-     const dataErr = {};
-     const minErr = {};
-     const maxErr = {};
+    const dataErr = {};
+    const minErr = {};
+    const maxErr = {};
 
-     let isValid = true;
+    let isValid = true;
 
-     if(  !data ){
-       dataErr.required = "Required"
-       isValid=false;
-     }
-
-     if( validation === "integer-range" || validation === "decimal-range"){
-       if(data < min){
-         dataErr.minimum =  `Must be at least ${min} `
-         isValid = false;
-       }
-      else if(data  > max){
-         dataErr.maximum = `Must be equal or lower than ${max} `
-         isValid = false;
-       }
-     }
-     if( validation === "integer-range" || validation === "decimal-range"){
-        if(!min){
-          minErr.required = "Required"
-          isValid=false;
-        }
-       else if(!max){
-          maxErr.required =  "Required"
-          isValid=false;
-        }
-     }
-     if(validation === "decimal-range" || validation === "decimal"){
-       if(data%1 === 0){
-         console.log(data)
-         dataErr.decimal = "Must be decimal!"
-         isValid=false;
-       }
-     else if(min%1 === 0){
-         console.log(min)
-         minErr.decimal = "Must be decimal!"
-         isValid=false;
-      }
-     else if(max%1 === 0){
-        console.log(max)
-        maxErr.decimal = "Must be decimal!"
-        isValid=false;
-      }
-     }
-    //  if(max < min ){
-    //    maxErr.bigger = "Max must be greater than min"
-    //    isValid=false;
-    //  }
-     if(validation === "integer-range" || validation === "integer"){
-      console.log(data)
-     
-      if(data%1 !== 0){
-        console.log(data)
-        dataErr.decimal = "Must be integer!"
-        isValid=false;
-      }
-     else if(min%1 !== 0){
-      console.log(min)
-       minErr.decimal = "Must be integer!"
-       isValid=false;
-     }
-   else if(max%1 !== 0){
-       console.log(max)
-       maxErr.decimal = "Must be integer!"
-       isValid=false;
-     }
-    }
-    if(parseFloat(max)  < parseFloat(min) ){
-      console.log(max)
-      console.log(min)
-      maxErr.bigger = "Max must be greater than min"
+    if(  !data ){
+      dataErr.required = "Required"
       isValid=false;
     }
-     setDataErr(dataErr);
-     setMaxErr(maxErr);
-     setMinErr(minErr);
 
-     return isValid;
-  }
+    if( validation === "integer-range" || validation === "decimal-range"){
+      if(data <= min){
+        dataErr.minimum =  `Must be at least ${min} `
+        isValid = false;
+      }
+     else if(data  >= max){
+        dataErr.maximum = `Must be equal or lower than ${max} `
+        isValid = false;
+      }
+    }
+    if( validation === "integer-range" || validation === "decimal-range"){
+       if(!min){
+         minErr.required = "Required"
+         isValid=false;
+       }
+      else if(!max){
+         maxErr.required =  "Required"
+         isValid=false;
+       }
+    }
+    if(validation === "decimal-range" || validation === "decimal"){
+      if(data%1 === 0){
+        console.log(data)
+        dataErr.decimal = "Must be decimal!"
+        isValid=false;
+      }
+    else if(min%1 === 0){
+        console.log(min)
+        minErr.decimal = "Must be decimal!"
+        isValid=false;
+     }
+    else if(max%1 === 0){
+       console.log(max)
+       maxErr.decimal = "Must be decimal!"
+       isValid=false;
+     }
+    }
+   //  if(max < min ){
+   //    maxErr.bigger = "Max must be greater than min"
+   //    isValid=false;
+   //  }
+    if(validation === "integer-range" || validation === "integer"){
+     console.log(data)
+    
+     if(data%1 !== 0){
+       console.log(data)
+       dataErr.decimal = "Must be integer!"
+       isValid=false;
+     }
+    else if(min%1 !== 0){
+     console.log(min)
+      minErr.decimal = "Must be integer!"
+      isValid=false;
+    }
+  else if(max%1 !== 0){
+      console.log(max)
+      maxErr.decimal = "Must be integer!"
+      isValid=false;
+    }
+   }
+   if(parseFloat(max)  < parseFloat(min) ){
+     console.log(max)
+     console.log(min)
+     maxErr.bigger = "Max must be greater than min"
+     isValid=false;
+   }
+    setDataErr(dataErr);
+    setMaxErr(maxErr);
+    setMinErr(minErr);
+
+    return isValid;
+ }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // setIsSubmitting(true);
+
+    // const isValid = formValidation();
+    // console.log(isValid)
+    // if(isValid){
+    //   setIsSubmitting(false);
+    //   console.log(data)
+      
+    //   setData("");
+    //   setMin(1);
+    //   setMax(32);
+     
+    // }
+    
+    // setIsSubmitting(true)
+  };
+
+  // theme forest -- code canyon
  
   return (
     <>
       <h1>Survey Form</h1>
-
-      <form noValidate onSubmit={handleSubmit}>
-        <div>
+      <div>
           <label htmlFor="name">Validation Type</label>
           <div>
             <select
@@ -193,7 +200,6 @@ const SurveyForm = () => {
                 id="min"
                 name="min"
                 type="number"
-                min="0"
                 pattern={shape.pattern}
                 step={shape.step}
                 inputMode="numeric"
@@ -212,11 +218,11 @@ const SurveyForm = () => {
                 id="max"
                 name="max"
                 type="number"
-                min="0"
                 pattern={shape.pattern}
                 step={shape.step}
                 inputMode="numeric"
                 onChange={handleMaxChange}
+                
                 value={max}
                 placeholder="Maximum"
               />
@@ -226,6 +232,9 @@ const SurveyForm = () => {
             </div>
           ) : null}
           <br />
+          </div>
+      <form  onSubmit={handleSubmit}>
+        
 
           <div className="data-wrapper">
             <label htmlFor="data">Data Entry</label>
@@ -237,13 +246,15 @@ const SurveyForm = () => {
               id="data"
               name="data"
               type="number"
-              min="0"
+              {...validation ==="integer-range" || validation === "decimal-range" ?min={min} :null} 
+              {...validation ==="integer-range" || validation === "decimal-range" ?max={max} :null} 
               pattern={shape.pattern}
               step={shape.step}
               inputMode="numeric"
               onChange={handleDataChange}
               value={data}
               placeholder="Enter data"
+              style={{width:120}}
             />
             <br/>
             <small id="dataHelp" className="form-text text-muted">
@@ -254,7 +265,7 @@ const SurveyForm = () => {
                 return <div key={key} style={{color:"red"}}>{dataErr[key]}</div>
               })}
           </div>
-        </div>
+       
         <br />
         <button className="button" type="submit" disabled={!isSubmitting}>
           Submit
